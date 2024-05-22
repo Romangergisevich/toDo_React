64dbbf3d16358deda511a5ea2081935ab43a9884
unknown <Romangergisevich@gmail.com>
2024-05-22 22:11:08 +0300

    home page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import pages from "vite-plugin-pages";
 
 export default defineConfig({
-  base: "/toDo_React/home",
+  base: "/toDo_React/",
   plugins: [pages()],
 });
