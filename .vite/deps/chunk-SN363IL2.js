1e2d079a49fb08abe36a88dce3178277f2d94ff3
unknown <Romangergisevich@gmail.com>
2024-05-22 21:25:06 +0300

    optimize libs

---
+++ .vite/deps/chunk-SN363IL2.js
@@ -0,0 +1,13 @@
+import {
+  capitalize,
+  init_capitalize
+} from "./chunk-GDVIWPRD.js";
+
+// node_modules/@mui/material/utils/capitalize.js
+init_capitalize();
+var capitalize_default = capitalize;
+
+export {
+  capitalize_default
+};
+//# sourceMappingURL=chunk-SN363IL2.js.map
